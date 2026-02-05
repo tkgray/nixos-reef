ce00c452fbbd0e87d2ad0f74757154f174d8a251
tkgray <tom@tkgray.com>
2026-02-05 15:48:50 -0800

    Initial NixOS configuration with flake support

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.reef = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
